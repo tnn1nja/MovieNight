c15be4b6de452ed399afb5f75e6e16472a4547fd
tnn1nja <thenonsenseninja@gmail.com>
2024-02-15 11:08:45 +0000

    Enviroment Configuration

---
+++ src/main/java/net/tnn1nja/Init.java
@@ -0,0 +1,10 @@
+package net.tnn1nja;
+
+public class Init {
+
+    static Main main = new Main();
+
+    public static void main(String[] args) {
+        main.onStart();
+    }
+}
---
+++ src/main/java/net/tnn1nja/Main.java
@@ -0,0 +1,9 @@
+package net.tnn1nja;
+
+public class Main {
+
+    public void onStart(){
+        System.out.println("Hello World");
+    }
+
+}
