6a272679ff1002fce874c7dc2267bf1690261346
tnn1nja <thenonsenseninja@gmail.com>
2024-02-16 10:27:19 +0000

    Logger Utils Test 2

--- src/main/java/net/tnn1nja/movieNight/Main.java
+++ src/main/java/net/tnn1nja/movieNight/Main.java
@@ -2,10 +2,21 @@ package net.tnn1nja.movieNight;
 
 import net.tnn1nja.movieNight.utils.LogHandler;
 
+import java.util.logging.Logger;
+
 public class Main {
 
+    Logger log = Logger.getLogger("mainLogger");
+
     public void onStart(){
-        LogHandler.setup();
+
+        LogHandler.setup(log);
+
+        log.info("Info Log!");
+        log.warning("Warning Log!");
+        log.severe("Severe Log!");
+        log.fine("Debug Log!");
+
         System.out.println("Hello World");
     }
 
--- src/main/java/net/tnn1nja/movieNight/utils/LogHandler.java
+++ src/main/java/net/tnn1nja/movieNight/utils/LogHandler.java
@@ -1,11 +1,33 @@
 package net.tnn1nja.movieNight.utils;
 
-import java.util.logging.Logger;
+import java.util.logging.*;
 
-public class LogHandler {
-    static Logger log = Logger.getLogger("mainLogger");
+public class LogHandler extends Formatter {
 
-    public static void setup(){
-        log.info("Hello World!");
+    public static void setup(Logger log){
+        //Base Settings
+        log.setUseParentHandlers(false);
+
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setFormatter(new LogHandler());
+        log.addHandler(ch);
+    }
+
+    @Override
+    public String format(LogRecord record) {
+        //Create and Format Strings
+        String recordLevel = formatLevel(record.getLevel());
+
+        //Return Final Concatenated String
+        return "[" + recordLevel + "] " + record.getMessage() + "\n";
+    }
+
+    //Remaps Fine, Finer and Finest Levels to "Debug"
+    public static String formatLevel(Level recordLevel){
+        if(recordLevel.intValue() < Level.INFO.intValue()){
+            return "DEBUG";
+        }else{
+            return recordLevel.getName();
+        }
     }
 }
