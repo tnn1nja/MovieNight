8b0f00eac86728acc5c7d76583e95168dccd2e53
tnn1nja <thenonsenseninja@gmail.com>
2024-02-19 11:08:17 +0000

    Positive SQLite Test.

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -cp D:\Coding\WBS\MovieNight\codebase\target\movieNight-1.0.jar net.tnn1nja.movieNight.Init
+java -cp .\libs\sqlite-jdbc-3.39.2.1.jar;D:\Coding\WBS\MovieNight\codebase\target\movieNight-1.0.jar net.tnn1nja.movieNight.Init
